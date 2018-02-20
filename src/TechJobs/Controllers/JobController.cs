e21b3549276febc3dc043d9683ea68f0e01212b2
Jeremy Maldonado <jeremymaldonado99@hotmail.com>
2018-02-19 22:59:07 -0600

    Started work on TechJobsOO

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -19,6 +20,7 @@ namespace TechJobs.Controllers
         public IActionResult Index(int id)
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
+            Models.Job selectedJob = jobData.Find(id);
 
             return View();
         }
@@ -35,6 +37,18 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    
+                };
+
+                jobData.Jobs.Add(newJob);
+                return Redirect("/Job?id=" + newJob.ID.ToString());
+            }
 
             return View(newJobViewModel);
         }
